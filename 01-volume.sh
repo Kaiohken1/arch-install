86d943b9102598d0fe4f8f6ba256d8233924ed9c
Kaiohken1 <109998109+Kaiohken1@users.noreply.github.com>
2025-02-13 21:12:22 +0100

    correction sur les partitions

--- 01-volume.sh
+++ 01-volume.sh
@@ -1,23 +1,23 @@
+#!/bin/bash
 
-source config.sh
-
+source ./config.sh
 
 echo "Création des partitions sur $DISK"
 
 parted -s $DISK mklabel gpt
 
-parted -s $DISK mkpart ESP fat32 1MiB 512MiB
+parted -s $DISK mkpart primary fat32 1MiB 512MiB
 parted -s $DISK set 1 esp on
 
-parted -s $DISK mkpart primary linux-swap 512MiB 8.5GiB
-
-parted -s $DISK mkpart primary 8.5GiB 100%
+parted -s $DISK mkpart primary ext4 512MiB 100%
 
 partprobe $DISK
-sleep 2
 
-pvcreate $ROOT_PARTITION
-vgcreate $VOLUME_GROUP $ROOT_PARTITION
+echo $PASSWORD | cryptsetup luksFormat --batch-mode $ROOT_PARTITION
+echo $PASSWORD | cryptsetup open $ROOT_PARTITION cryptlvm
+
+pvcreate /dev/mapper/cryptlvm
+vgcreate $VOLUME_GROUP /dev/mapper/cryptlvm
 
 lvcreate -L 8G -n lvswap $VOLUME_GROUP
 lvcreate -L 40G -n lvrootfs $VOLUME_GROUP
@@ -26,40 +26,35 @@ lvcreate -L 10G -n lvencrypteddata $VOLUME_GROUP
 lvcreate -l 50%FREE -n lvhomeu $VOLUME_GROUP
 lvcreate -l 100%FREE -n lvhomesu $VOLUME_GROUP
 
-cryptsetup luksFormat /dev/$VOLUME_GROUP/lvrootfs
-cryptsetup open /dev/$VOLUME_GROUP/lvrootfs root
-
-cryptsetup luksFormat --type luks1 /dev/$VOLUME_GROUP/lvswap
-cryptsetup open /dev/$VOLUME_GROUP/lvswap swap
-
-cryptsetup luksFormat /dev/$VOLUME_GROUP/lvencrypteddata
-cryptsetup open /dev/$VOLUME_GROUP/lvencrypteddata dedicated_space
-
-mkfs.ext4 /dev/mapper/root
+mkfs.ext4 /dev/$VOLUME_GROUP/lvrootfs
 mkfs.ext4 /dev/$VOLUME_GROUP/lvhomeu
 mkfs.ext4 /dev/$VOLUME_GROUP/lvhomesu
 mkfs.ext4 /dev/$VOLUME_GROUP/lvvmdata
-mkfs.ext4 /dev/mapper/dedicated_space
-mkswap /dev/mapper/swap
+mkfs.ext4 /dev/$VOLUME_GROUP/lvencrypteddata
+mkswap /dev/$VOLUME_GROUP/lvswap
 
-mkfs.fat -F32 $EFI_PARTITION
-
-mount /dev/mapper/root /mnt
-mount --mkdir $EFI_PARTITION /mnt/boot
+mount /dev/$VOLUME_GROUP/lvrootfs /mnt
 
 mkdir -p /mnt/home/user
-mount /dev/mapper/archvg-lvhomeu /mnt/home/user
+mount /dev/$VOLUME_GROUP/lvhomeu /mnt/home/user
 
-mkdir -p /mnt/home/superuser
-mount /dev/mapper/archvg-lvhomesu /mnt/home/superuser
+mkdir -p /mnt/home/son_user
+mount /dev/$VOLUME_GROUP/lvhomesu /mnt/home/son_user
 
-mkdir -p /mnt/var/lib/libvirt
-mount /dev/mapper/archvg-lvvmdata /mnt/var/lib/libvirt
+mkdir -p /mnt/var/vm
+mount /dev/$VOLUME_GROUP/lvvmdata /mnt/var/vm
 
 mkdir -p /mnt/dedicated_space
-mount /dev/mapper/dedicated_space /mnt/dedicated_space
+mount /dev/$VOLUME_GROUP/lvencrypteddata /mnt/dedicated_space
 
-swapon /dev/mapper/swap
+swapon /dev/$VOLUME_GROUP/lvswap
 
+mkfs.fat -F32 $EFI_PARTITION
+mkdir -p /mnt/boot
+mount $EFI_PARTITION /mnt/boot
+
+mkdir -p /mnt/etc
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "/dev/mapper/dedicated_space  none  ext4  defaults  0  0" >> /mnt/etc/fstab
+echo "/dev/mapper/lvencrypteddata  none  ext4  defaults  0  0" >> /mnt/etc/fstab
+
+echo "HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)" > /mnt/etc/mkinitcpio.conf
