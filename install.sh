ea0b8a569592671110b6dfc4951c39cac5669554
LibelleQ <148550971+LibelleQ@users.noreply.github.com>
2025-02-13 18:50:27 +0100

    [TEST] volume.sh

--- install.sh
+++ install.sh
@@ -2,30 +2,22 @@
 set -x
 set -eo pipefail
 
-chmod 777 script/*
+chmod +x script/*.sh
 
-if [ ! -f script/config.sh ]; then
-    echo "Error: config.sh not found!"
-    exit 1
-fi
-source script/config.sh
+# Vérifier la présence du fichier config.sh
+# if [ ! -f script/config.sh ]; then
+#    echo "Error: config.sh not found!"
+#    exit 1
+# fi
+# source script/config.sh
 
 echo -ne "
 Starting...
 "
 
-# Execute shared folder script
-#if [ ! -f script/09-shared_folder.sh ]; then
-#    echo "Error: 09-shared_folder.sh not found!"
-#    exit 1
-#fi
-#./script/09-shared_folder.sh
-
-
-# Execute volume script
-
-if [ ! -f 01-volume.sh ]; then
-    echo "Error: 09-shared_folder.sh not found!"
+# Exécuter le script 01-volume.sh
+if [ ! -f script/01-volume.sh ]; then
+    echo "Error: 01-volume.sh not found!"
     exit 1
 fi
-./01-volume.sh
+(./script/01-volume.sh)
