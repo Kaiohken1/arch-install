71ce0630f2e970a8b32ba4a3129889d81d37cb8d
LibelleQ <148550971+LibelleQ@users.noreply.github.com>
2025-02-13 18:27:16 +0100

    [ADD] add volume script in install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
-source script/config.sh
-
+#!/bin/bash
 set -x
-
 set -eo pipefail
+
+
+if [ ! -f script/config.sh ]; then
+    echo "Error: config.sh not found!"
+    exit 1
+fi
+source script/config.sh
 
 echo -ne "
 Starting...
 "
 
-./script/shared_folder.sh
+# Execute shared folder script
+#if [ ! -f script/09-shared_folder.sh ]; then
+#    echo "Error: 09-shared_folder.sh not found!"
+#    exit 1
+#fi
+#./script/09-shared_folder.sh
+
+
+# Execute volume script
+
+if [ ! -f script/01-volume.sh ]; then
+    echo "Error: 09-shared_folder.sh not found!"
+    exit 1
+fi
+./script/01-volume.sh
