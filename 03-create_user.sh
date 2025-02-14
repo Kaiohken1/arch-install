4fff63dfa35f8fb849f897fc8ac5dd0aa878983b
Kaiohken1 <109998109+Kaiohken1@users.noreply.github.com>
2025-02-15 00:06:32 +0100

    add correct rights

--- 03-create_user.sh
+++ 03-create_user.sh
@@ -4,6 +4,9 @@ arch-chroot /mnt /bin/bash <<EOF
 useradd -G wheel $ADMIN_USER
 useradd -G users $USER
 
+chown -R "$USER:users" /home/"$USER"
+chown -R "$ADMIN_USER:wheel" /home/"$ADMIN_USER"
+
 echo -e "$PASSWORD\n$PASSWORD" | passwd "$ADMIN_USER"
 echo -e "$PASSWORD\n$PASSWORD" | passwd "$USER"
 echo -e "$PASSWORD\n$PASSWORD" | passwd
